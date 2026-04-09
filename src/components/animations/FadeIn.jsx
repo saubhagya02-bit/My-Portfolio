fc4fb149c143797648c15f1fba389bc8f5127906
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-04-09 13:24:35 +0530

    add reusable FadeIn and ScrollReveal components

--- src/components/animations/FadeIn.jsx
+++ src/components/animations/FadeIn.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+const FadeIn = ({ children, delay = 0, direction = "up" }) => {
+  let translateClass = "";
 
-function FadeIn() {
+  switch (direction) {
+    case "up":
+      translateClass = "translate-y-10";
+      break;
+    case "down":
+      translateClass = "-translate-y-10";
+      break;
+    case "left":
+      translateClass = "translate-x-10";
+      break;
+    case "right":
+      translateClass = "-translate-x-10";
+      break;
+    default:
+      translateClass = "translate-y-10";
+  }
+
   return (
-    <div>FadeIn</div>
-  )
-}
+    <div
+      className={`opacity-0 ${translateClass} animate-fadeIn`}
+      style={{
+        animationDelay: `${delay}ms`,
+        animationFillMode: "forwards",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
-export default FadeIn
+export default FadeIn;
--- src/components/animations/ScrollReveal.jsx
+++ src/components/animations/ScrollReveal.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import useScrollReveal from "../../hooks/useScrollReveal";
 
-function ScrollReveal() {
+const ScrollReveal = ({ children }) => {
+  const [ref, visible] = useScrollReveal();
+
   return (
-    <div>ScrollReveal</div>
-  )
-}
+    <div
+      ref={ref}
+      className={`transition-all duration-700 ${
+        visible
+          ? "opacity-100 translate-y-0"
+          : "opacity-0 translate-y-12"
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
 
-export default ScrollReveal
+export default ScrollReveal;
